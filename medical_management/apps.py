4a0cd4883eede8b46e6d3b85b3c03da7c1c5a069
ss-sahoo <shradhamilu160@gmail.com>
2025-07-15 17:44:01 +0530

    changes

--- medical_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedicalManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medical_management'
-    verbose_name = 'Medical Management'
